bd78fe643e5b211f96e57db94a4aefdf5f83be99
HitMargin <2228293026@qq.com>
2026-09-19 14:10:07 +0800

    chore: bump version to 1.1.6 and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## v1.1.6 — 2026.09.19
 
 ### Features
 
--- JipperOverlayer/Properties/AssemblyInfo.cs
+++ JipperOverlayer/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("38AEB58B-A5DE-44BF-AC1E-180B20543F5F")]
-[assembly: AssemblyVersion("1.1.5.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
+[assembly: AssemblyVersion("1.1.6.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
